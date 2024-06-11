783f778ce23c64b0ac2d7080af6a0f86d096118a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-11 20:51:58 +0430

    graphql products

---
+++ products/products.modal.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 'redshoe',
+        description: 'Red shoe',
+        price: 42.12,
+    },
+    {
+        id: 'bluejeans',
+        description: 'Blue Jeans',
+        price: 55.55,
+    }
+]
