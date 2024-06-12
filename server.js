b88d607a8fe1743cbad536b72e287e5435a89e92
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-12 20:51:00 +0430

    set up server

--- server.js
+++ server.js
@@ -6,21 +6,17 @@ const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'))
 
 const schema = makeExecutableSchema({
-    typeDefs: typesArray
+    typeDefs: typesArray,
+    resolvers: resolversArray,
 })
-
-const root = {
-    products: require('./products/products.modal'),
-    orders: require('./orders/orders.modal')
-}
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
 }));
 
