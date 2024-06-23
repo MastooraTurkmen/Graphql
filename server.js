5d6cb5e7934496dd1f9470afc507b07dfa3a171f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-23 17:30:51 +0430

    change to apollo graphql

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const path = require('path')
 const express = require('express');
 
-const { graphqlHTTP } = require('express-graphql');
+const { ApolloServer } = require('apollo-server-express')
+
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'))
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray,
-})
+async function startApolloServer() {
+    const app = express();
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray,
+    });
 
-const app = express();
+    const server = new ApolloServer({
+        schema
+    })
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-}));
+    await server.start();
+    server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen(3000, () => {
-    console.log('Running GraphQL server...');
-});
+    app.listen(3000, () => {
+        console.log('Running GraphQL server...');
+    });
+}
+
+startApolloServer();
