c40a7c7dcbfe08cb9020c396ffb1ad08a83695da
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-23 17:00:44 +0430

    add new product

---
+++ products/products.model.js
@@ -0,0 +1,64 @@
+const products = [
+    {
+        id: 'redshoe',
+        description: 'Red shoe',
+        price: 42.12,
+        reviews: [],
+    },
+    {
+        id: 'bluejeans',
+        description: 'Blue Jeans',
+        price: 55.55,
+        reviews: [],
+    }
+]
+
+function getAllProducts() {
+    return products;
+}
+
+function getProductsByPrice(min, max) {
+    return products.filter((product) => {
+        return products.price >= min && product.price < max;
+    })
+}
+
+function getProductById(id) {
+    return products.find((product) => {
+        return product.id === id
+    })
+}
+
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: [],
+    };
+
+    products.push(newProduct);
+    return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+    const matchedProduct = getProductById(id)
+
+    if (matchedProduct) {
+        const newProductReview = {
+            rating,
+            comment,
+        };
+        matchedProduct.reviews.push(newProductReview);
+
+        return newProductReview;
+    }
+}
+
+module.exports = {
+    getAllProducts,
+    getProductsByPrice,
+    getProductById,
+    addNewProduct,
+    addNewProductReview
+};
